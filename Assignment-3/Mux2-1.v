f76cb7deb12984655f5e086f5375bb076b757908
AshrithaMuniyoor-Sahyadri-ECE <ashrithamuniyoor@gmail.com>
2025-06-01 21:23:23 +0530

    Create Mux2-1.v

---
+++ Assignment-3/Mux2-1.v
@@ -0,0 +1,29 @@
+Design Code:
+module mux_dff_feedback (
+    input  wire clk,
+    input  wire I1,
+    input  wire S,
+    output reg  Q
+);
+    wire D;
+    assign D = (S == 1'b0) ? Q : I1;
+
+    always @(posedge clk) begin
+        Q <= D;
+    end
+endmodule
+
+Testbench:
+module mux_dff_feedback (
+    input  wire clk,
+    input  wire I1,
+    input  wire S,
+    output reg  Q
+);
+    wire D;
+    assign D = (S == 1'b0) ? Q : I1;
+
+    always @(posedge clk) begin
+        Q <= D;
+    end
+endmodule
