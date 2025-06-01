ae7378ce0e446c892ed261b8244b71870d9c1de8
AshrithaMuniyoor-Sahyadri-ECE <ashrithamuniyoor@gmail.com>
2025-06-01 21:00:39 +0530

    FA.v

--- Assignment-2/FA.v
+++ Assignment-2/FA.v
@@ -11,13 +11,33 @@ module full_adder (
 endmodule
 
 Testbench:
-module full_adder (
-    input A,
-    input B,
-    input Cin,
-    output Sum,
-    output Cout
-);
-    assign Sum = A ^ B ^ Cin;
-    assign Cout = (A & B) | (B & Cin) | (A & Cin);
+`timescale 1ns/1ps
+
+module full_adder_tb;
+    reg A_tb, B_tb, Cin_tb;
+    wire Sum_tb, Cout_tb;
+
+    full_adder uut (
+      .A(A_tb),
+        .B(B_tb),
+        .Cin(Cin_tb),
+        .Sum(Sum_tb),
+        .Cout(Cout_tb)
+    );
+
+    initial begin
+      $dumpfile("dump.vcd");
+      $dumpvars;
+        A_tb = 0; B_tb = 0; Cin_tb = 0; #10;
+        A_tb = 0; B_tb = 0; Cin_tb = 1; #10;
+        A_tb = 0; B_tb = 1; Cin_tb = 0; #10;
+        A_tb = 0; B_tb = 1; Cin_tb = 1; #10;
+        A_tb = 1; B_tb = 0; Cin_tb = 0; #10;
+        A_tb = 1; B_tb = 0; Cin_tb = 1; #10;
+        A_tb = 1; B_tb = 1; Cin_tb = 0; #10;
+        A_tb = 1; B_tb = 1; Cin_tb = 1; #10;
+
+        $finish;
+    end
 endmodule
+
