8e64d4abe195682b6b2409198a5670d1c3be2ac7
AshrithaMuniyoor-Sahyadri-ECE <ashrithamuniyoor@gmail.com>
2025-06-01 20:49:19 +0530

    Create FA.v

---
+++ Assignment-2/FA.v
@@ -0,0 +1,23 @@
+Design Code:
+module full_adder (
+    input A,
+    input B,
+    input Cin,
+    output Sum,
+    output Cout
+);
+    assign Sum = A ^ B ^ Cin;
+    assign Cout = (A & B) | (B & Cin) | (A & Cin);
+endmodule
+
+Testbench:
+module full_adder (
+    input A,
+    input B,
+    input Cin,
+    output Sum,
+    output Cout
+);
+    assign Sum = A ^ B ^ Cin;
+    assign Cout = (A & B) | (B & Cin) | (A & Cin);
+endmodule
